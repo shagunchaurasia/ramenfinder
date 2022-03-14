35a18e1c225ce19f3e60f0f623a26b8f4ba14915
shagunchaurasia <chaurasia.shagun@gmail.com>
2022-03-14 21:54:25 +0900

    Card component added with classes

--- src/components/Card/Card.component.tsx
+++ src/components/Card/Card.component.tsx
@@ -1,13 +1,35 @@
 import React from "react";
-import './Card.style.scss';
+import "./Card.style.scss";
 
-export const Card = (): JSX.Element => {
+interface CardProps {
+  id: string;
+  allProps: {
+    name: string;
+    address: string;
+    catch: string;
+    logo_image: string;
+    photo: { pc: { l: string } };
+  };
+}
 
-    return(
-        <div>This will be the card component</div>
-    )
-    
+export const Card = ({id,allProps}: CardProps): JSX.Element => {
+  return (
+    <div className="card-container " id={id}>
+      <div className="details">
+        <div className="name-address">
+          <div className="title">{allProps.name}</div>
+          <div className="subtitle">{allProps.address}</div>
+        </div>
+
+        <div className="rating-star"></div>
+      </div>
+
+      <div className="image-container">
+        <img src={allProps.photo.pc.l} alt="" className="image" />
+      </div>
+      <div className="description">{allProps.catch}</div>
+
+      <button className="findMore">Find Out More</button>
+    </div>
+  );
 };
-
-
-
