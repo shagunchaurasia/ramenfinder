3f93efaaac13a4a61f8b25bb3c2388a8826af6f4
shagunchaurasia <chaurasia.shagun@gmail.com>
2022-03-14 21:55:57 +0900

    Search box field added

---
+++ src/components/SearchBox/SearchBox.component.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface SearchBoxProps {
+  //   onChangeHandler: React.InputHTMLAttributes<HTMLInputElement>.onChange?: React.ChangeEventHandler<HTMLInputElement> | undefined;
+  onChangeHandler: any;
+  placeholderText: string;
+  className: string;
+}
+
+export const SearchBox = (props: SearchBoxProps): JSX.Element => {
+  return (
+    <input
+      className={`search-box ${props.className}`}
+      type="search"
+      placeholder={props.placeholderText}
+      onChange={props.onChangeHandler}
+    />
+  );
+};
