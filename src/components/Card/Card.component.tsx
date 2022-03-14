1e34833f288b84c233fc3503195ba0ad9b89ddce
shagunchaurasia <chaurasia.shagun@gmail.com>
2022-03-14 19:13:09 +0900

    Initial setup for components

---
+++ src/components/Card/Card.component.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Card.style.scss';
+
+export const Card = (): JSX.Element => {
+
+    return(
+        <div>This will be the card component</div>
+    )
+    
+};
+
+
+
