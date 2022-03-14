786e3303b3514dc970053c2e5d41b07c4ee634e1
shagunchaurasia <chaurasia.shagun@gmail.com>
2022-03-14 19:33:09 +0900

    Bootstrap added and base homepage set up

---
+++ src/pages/Homepage/Homepage.component.tsx
@@ -0,0 +1,13 @@
+import Card from 'components/Card';
+import React from 'react';
+import './Homepage.style.scss';
+
+export const Homepage = ():JSX.Element =>{
+
+    return (
+        <div>This will be homepage listing
+            <Card></Card>
+        </div>
+    )
+
+}
