d908bc6b8c3e51e7dc737351858e46aeafc1ebee
shagunchaurasia <chaurasia.shagun@gmail.com>
2022-03-20 19:51:49 +0900

    Cleaned package.json file

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,9 @@
-let env = process.env;
+let { REACT_APP_SERVICE_BASE_URL} = process.env;
+
 const config = {
   deviceType: "web",
   service: {
-    BASE_URL: env.REACT_APP_SERVICE_BASE_URL,
+    BASE_URL:  REACT_APP_SERVICE_BASE_URL,
   },
 };
 
